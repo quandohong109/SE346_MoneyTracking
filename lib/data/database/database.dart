70c7d1f016fa2dbd810d9dba1cf71cb901450eef
Terry <22521474@gm.uit.edu.vn>
2024-06-24 00:38:50 +0700

    Update database.dart

--- lib/data/database/database.dart
+++ lib/data/database/database.dart
@@ -1,122 +1,151 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
-import '../../objects/dtos/category_dto.dart';
-import '../../objects/models/category_model.dart';
-import '../database/database.dart';
-import '../firebase/firebase.dart';
+import 'package:money_tracking/data/firebase/firebase.dart';
+import 'package:money_tracking/functions/converter.dart';
+import 'package:money_tracking/objects/models/category_model.dart';
 import '../../functions/getdata.dart';
+import '../../objects/models/icon_type.dart';
+import '../../objects/models/transaction_model.dart';
+import '../../objects/models/wallet_model.dart';
 
-class CategoryBUS {
-  static Future<void> addCategoryToFirestore(CategoryModel category) async {
+class Database {
+  static final Database _database = Database._internal();
+
+  factory Database() {
+    return _database;
+  }
+
+  Database._internal();
+
+  List<IconType> iconTypeList = [
+    IconType(id: 1, icon: Icons.cake),
+    IconType(id: 2, icon: Icons.savings),
+    IconType(id: 3, icon: Icons.build),
+    IconType(id: 4, icon: Icons.pets),
+    IconType(id: 5, icon: Icons.phone),
+    IconType(id: 6, icon: Icons.sports_esports),
+    IconType(id: 7, icon: Icons.router),
+    IconType(id: 8, icon: Icons.outdoor_grill),
+    IconType(id: 9, icon: Icons.restaurant),
+    IconType(id: 10, icon: Icons.shopping_cart),
+    IconType(id: 11, icon: Icons.train),
+    IconType(id: 12, icon: Icons.local_gas_station),
+    IconType(id: 13, icon: Icons.card_travel),
+    IconType(id: 14, icon: Icons.garage),
+    IconType(id: 15, icon: Icons.home_work),
+    IconType(id: 16, icon: Icons.add_box),
+    IconType(id: 17, icon: Icons.money),
+    IconType(id: 18, icon: Icons.electric_bolt),
+    IconType(id: 19, icon: Icons.water_drop),
+    IconType(id: 20, icon: Icons.local_library),
+  ];
+
+  List<CategoryModel> categoryList = [];
+  List<WalletModel> walletList = [];
+  List<TransactionModel> transactionList = [];
+
+  // void updateCategoryList() {
+  //   categoryList = Firebase().categoryList.map((e) {
+  //     return CategoryModel(
+  //       id: e.id,
+  //       name: e.name,
+  //       iconType: iconTypeList.firstWhere((element) => element.id == e.iconID),
+  //       isIncome: e.isIncome,
+  //       color: Color.fromRGBO(e.red, e.green, e.blue, e.opacity),
+  //     );
+  //   }).toList();
+  // }
+
+  void updateWalletList() {
+    walletList = Firebase().walletList.map((e) {
+      return WalletModel(
+        id: e.id,
+        name: e.name,
+        icon: iconTypeList.firstWhere((element) => element.id == e.iconID),
+        balance: e.balance,
+      );
+    }).toList();
+  }
+
+  // void updateTransactionList() {
+  //   updateCategoryList();
+  //   updateWalletList();
+  //   transactionList = Firebase().transactionList.map((e) {
+  //     return TransactionModel(
+  //       id: e.id,
+  //       category: categoryList.firstWhere((element) => element.id == e.categoryID),
+  //       wallet: walletList.firstWhere((element) => element.id == e.walletID),
+  //       date: Converter.toDateTime(e.date),
+  //       note: e.note,
+  //       amount: e.amount,
+  //       isExpanded: false,
+  //     );
+  //   }).toList();
+  // }
+
+  Future<void> updateCategoryListFromFirestore() async {
     try {
-      QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('categories').orderBy('id', descending: true).limit(1).get();
-      int newId = 1; // Default value
-      if (snapshot.docs.isNotEmpty) {
-        var data = snapshot.docs.first.data() as Map<String, dynamic>;
-        int maxId = data['id'];
-        newId = maxId + 1;
-      }
-
-      // Now use newId for the new category
-      await FirebaseFirestore.instance.collection('categories').add({
-        'id': newId,
-        'name': category.name,
-        'iconID': category.iconType.id,
-        'isIncome': category.isIncome,
-        'red': category.color.red,
-        'green': category.color.green,
-        'blue': category.color.blue,
-        'opacity': category.color.opacity,
-        'userID': GetData.getUID(),
-      });
-
-      // Show success toast
-      // Fluttertoast.showToast(
-      //     msg: "Category added successfully",
-      //     toastLength: Toast.LENGTH_SHORT,
-      //     gravity: ToastGravity.BOTTOM,
-      //     timeInSecForIosWeb: 1,
-      //     backgroundColor: Colors.green,
-      //     textColor: Colors.white,
-      //     fontSize: 16.0
-      // );
-
-      await Database().updateCategoryListFromFirestore();
-    } catch (e) {
-      // If an error occurs, catch it and show an error toast
-      throw Exception("An error occurred - Category: ${e.toString()}");
+      final firestoreInstance = FirebaseFirestore.instance;
+      final QuerySnapshot querySnapshot = await firestoreInstance
+          .collection('categories')
+          .where('userID', isEqualTo: GetData.getUID())
+          .get();
+      categoryList = querySnapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return CategoryModel(
+          id: data['id'],
+          name: data['name'],
+          iconType: iconTypeList
+              .firstWhere((element) => element.id == data['iconID']),
+          isIncome: data['isIncome'],
+          color: Color.fromRGBO(
+              data['red'], data['green'], data['blue'], data['opacity']),
+        );
+      }).toList();
+    } on Exception {
+      rethrow;
     }
   }
 
-  static Future<void> addCategory(CategoryModel category) async {
-    Firebase().categoryList.add(
-      CategoryDTO(
-        id: category.id,
-        name: category.name,
-        iconID: category.iconType.id,
-        isIncome: category.isIncome,
-        red: category.color.red,
-        green: category.color.green,
-        blue: category.color.blue,
-        opacity: category.color.opacity,
-        userID: GetData.getUID(),
-      ),
-    );
-    await addCategoryToFirestore(category);
-    // await Database().updateCategoryList();
-    // await Database().updateCategoryListFromFirestore();
+  void updateWalletListFromFirestore() async {
+    final firestoreInstance = FirebaseFirestore.instance;
+    final QuerySnapshot querySnapshot = await firestoreInstance.collection('wallets')
+        .where('userID', isEqualTo: GetData.getUID())
+        .get();
+
+    walletList = querySnapshot.docs.map((doc) {
+      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+      return WalletModel(
+        id: data['id'],
+        name: data['name'],
+        icon: iconTypeList.firstWhere((element) => element.id == data['iconID']),
+        balance: BigInt.parse(data['balance']),
+      );
+    }).toList();
+    print(walletList);
   }
 
-  static Future<void> deleteCategoryFromFirestore(int categoryId) async {
-    try {
-      await FirebaseFirestore.instance.collection('categories')
-          .where('id', isEqualTo: categoryId)
-          .get()
-          .then((querySnapshot) {
-        querySnapshot.docs.forEach((doc) {
-          doc.reference.delete();
-        });
-      });
+  void updateTransactionListFromFirestore() async {
+    final firestoreInstance = FirebaseFirestore.instance;
+    final QuerySnapshot querySnapshot = await firestoreInstance.collection('transactions')
+        .where('userID', isEqualTo: GetData.getUID())
+        .get();
 
-      await Database().updateCategoryListFromFirestore();
-    } catch (e) {
-      throw Exception(e.toString());
-    }
-  }
+    updateCategoryListFromFirestore();
+    updateWalletListFromFirestore();
 
-  static Future<void> editCategoryInFirestore(CategoryModel category) async {
-    try {
-      await FirebaseFirestore.instance.collection('categories')
-          .where('id', isEqualTo: category.id)
-          .get()
-          .then((querySnapshot) {
-        for (var doc in querySnapshot.docs) {
-          doc.reference.update({
-            'name': category.name,
-            'iconID': category.iconType.id,
-            'isIncome': category.isIncome,
-            'red': category.color.red,
-            'green': category.color.green,
-            'blue': category.color.blue,
-            'opacity': category.color.opacity,
-            'userID': GetData.getUID(),
-          });
-        }
-      });
-      // Fluttertoast.showToast(
-      //     msg: "Category deleted successfully!",
-      //     toastLength: Toast.LENGTH_SHORT,
-      //     gravity: ToastGravity.BOTTOM,
-      //     timeInSecForIosWeb: 1,
-      //     backgroundColor: Colors.red,
-      //     textColor: Colors.white,
-      //     fontSize: 16.0
-      // );
-      await Database().updateCategoryListFromFirestore();
-    } catch (e) {
-      // If an error occurs, catch it and show an error toast
-      throw Exception("An error occurred - Category: ${e.toString()}");    
+    transactionList = querySnapshot.docs.map((doc) {
+      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+      return TransactionModel(
+        id: data['id'],
+        category: categoryList.firstWhere((element) => element.id == data['categoryID']),
+        wallet: walletList.firstWhere((element) => element.id == data['walletID']),
+        date: (data['date'] as Timestamp).toDate(),
+        note: data['note'],
+        amount: BigInt.parse(data['amount']),
+        isExpanded: false,
+      );
+    }).toList();
+    print(transactionList);
   }
 }
